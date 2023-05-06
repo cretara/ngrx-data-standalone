aff299b66f2f79e785c9e581951f3e894edb2dca
cretara <pierdanielecretara@lviiier.com>
2023-05-06 12:28:40 +0200

    First use of signal Updated .gitignore Added tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterModule, CommonModule]
 })
 export class AppComponent {
-  title = 'angular16-new-features';
+  public counter = signal(0);
+
+  counterAugment() {
+    this.counter.set(this.counter() + 1);
+  }
+
+  counterDecrement() {
+    this.counter.set(this.counter() - 1);
+  }
 }
