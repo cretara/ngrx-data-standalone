fc048ab0bdbc7b52eb30401d718793f12509f975
cretara <pierdanielecretara@lviiier.com>
2023-05-08 14:33:40 +0200

    First Cypress test on Angular 16

---
+++ cypress/e2e/app.component.spec.cy.ts
@@ -0,0 +1,7 @@
+import {AppComponent} from "../../src/app/app.component";
+
+describe("AppComponent", () => {
+  it("should mount AppComponent", () => {
+    cy.visit("http://localhost:4200/")
+  })
+})
