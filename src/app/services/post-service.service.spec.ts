e2e5c7e7252357600be0008018d5d687eb16505b
cretara <pierdanielecretara@lviiier.com>
2023-05-26 09:40:17 +0200

    Fixed Karma and Jasmine by removing Cypress from main project, it will be added later TODO fix import of PostListComponent in post-service.service.spec.ts

--- src/app/services/post-service.service.spec.ts
+++ src/app/services/post-service.service.spec.ts
@@ -1,27 +1,70 @@
 import {TestBed} from '@angular/core/testing';
-
 import {PostService} from './post.service';
+import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
+import {postResponseMock} from "../model/post";
+import {LoggerService} from "./logger.service";
+import {PostListComponent} from "../components/post-list/post-list.component";
+import {provideHttpClient} from "@angular/common/http";
+import {PostListMockComponent} from "../components/mock/post-list-mock/post-list-mock.component";
 
 describe('PostServiceService', () => {
   let postService: PostService;
-  let loggerService: any;
+  let loggerServiceSpy: any;
+  let httpTestingController: HttpTestingController;
+
+  const SINGLE_POST_ID = 63;
+  const SINGLE_POST_TITLE = 'voluptas blanditiis repellendus animi ducimus error sapiente et suscipit';
 
   beforeEach(() => {
-    loggerService = jasmine.createSpyObj("LoggerService", ["log"]);
+    loggerServiceSpy = jasmine.createSpyObj("LoggerService", ["log"]);
     TestBed.configureTestingModule({
       providers: [
         PostService,
         {
           provide: LoggerService,
-          useValue: loggerService
-        }
+          useValue: loggerServiceSpy
+        },
+        provideHttpClient(),
+        provideHttpClientTesting(),
       ]
-    });
+    }).overrideComponent(PostListComponent, {
+      remove: {imports: [PostListComponent]},
+      add: {imports: [PostListMockComponent]}
+    })
+      .compileComponents();
     postService = TestBed.inject(PostService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('PostService should log a certain message', () => {
     postService.logACertainMessage(postService.messageToLog);
     expect(postService).toBeTruthy();
   });
+
+  it('PostService should retrieve correctly post list', () => {
+    postService
+      .getPostsFromAPI()
+      .subscribe(
+        (responseGetPostAPI) => {
+          expect(responseGetPostAPI)
+            .withContext('No post is returned')
+            .toBeTruthy();
+          console.debug("responseGetPostAPI", responseGetPostAPI);
+          expect(responseGetPostAPI.length)
+            .withContext("Wrong number of posts retrieved from jsonplaceholder API")
+            .toBe(100);
+          const singlePost = responseGetPostAPI
+            .find((singlePost) => singlePost.id === SINGLE_POST_ID);
+          expect(singlePost?.body)
+            .withContext(`'singlePost title with id ${SINGLE_POST_ID} not equal to ${SINGLE_POST_TITLE} `)
+            .toBe(SINGLE_POST_TITLE)
+        }
+      );
+    const getPostRequest = httpTestingController.expectOne(`https://jsonplaceholder.typicode.com/posts`);
+    expect(getPostRequest.request.method).toEqual("GET");
+    getPostRequest.flush({
+      postResponseMock
+    });
+    httpTestingController.verify();
+  })
 });
