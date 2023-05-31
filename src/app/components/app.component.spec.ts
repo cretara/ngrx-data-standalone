f3955913837dfdf5bbedc63fcdd671df41a8c854
cretara <pierdanielecretara@lviiier.com>
2023-05-31 10:12:30 +0200

    Added counter.component.spec.ts unit tests Added setTimeout test in app.component.spec.ts

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, flush, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 import {provideHttpClient} from "@angular/common/http";
@@ -18,9 +18,15 @@ describe('AppComponent', () => {
     appComponent = appComponentFixture.componentInstance;
   });
 
-  it('should create the app', () => {
+  it('should create the AppComponent', () => {
     expect(appComponent).toBeTruthy();
   });
 
+  it('should test a setTimeout callback change value', fakeAsync(() => {
+    let testBooleanValue = false;
+    setTimeout(() => testBooleanValue = true, 1000);
+    flush();
+    expect(testBooleanValue).toBeTruthy();
+  }));
 
 });
--- src/app/components/counter/counter.component.spec.ts
+++ src/app/components/counter/counter.component.spec.ts
@@ -1,21 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CounterComponent } from './counter.component';
+import {CounterComponent} from './counter.component';
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('CounterComponent', () => {
-  let component: CounterComponent;
-  let fixture: ComponentFixture<CounterComponent>;
+  let counterComponent: CounterComponent;
+  let counterComponentFixture: ComponentFixture<CounterComponent>;
+  let counterComponentDebugElement: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [CounterComponent]
     });
-    fixture = TestBed.createComponent(CounterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    counterComponentFixture = TestBed.createComponent(CounterComponent);
+    counterComponent = counterComponentFixture.componentInstance;
+    counterComponentFixture.detectChanges();
+    counterComponentDebugElement = counterComponentFixture.debugElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create CounterComponent', () => {
+    expect(counterComponent)
+      .withContext("CounterComponent not created")
+      .toBeTruthy();
+  });
+
+  it('should increment counter by one', () => {
+    const COUNTER_INITIAL_VALUE = 0;
+    counterComponent.counter.set(COUNTER_INITIAL_VALUE);
+    const augmentButton = counterComponentDebugElement.query(By.css('#counter-augment'));
+    augmentButton.nativeElement.click();
+    counterComponentFixture.detectChanges();
+    expect(counterComponent.counter()).toBe(COUNTER_INITIAL_VALUE + 1);
+  });
+
+  it('should decrement counter by one', () => {
+    const COUNTER_INITIAL_VALUE = 1;
+    counterComponent.counter.set(COUNTER_INITIAL_VALUE);
+    const augmentButton = counterComponentDebugElement.query(By.css('#counter-decrement'));
+    augmentButton.nativeElement.click();
+    counterComponentFixture.detectChanges();
+    expect(counterComponent.counter()).toBe(COUNTER_INITIAL_VALUE - 1);
   });
 });
