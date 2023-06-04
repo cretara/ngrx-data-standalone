f48d21f622f37b07c8609d4e8a33f814608867cb
cretara <pierdanielecretara@lviiier.com>
2023-06-04 19:15:34 +0200

    Fixed unuseless blank spaces in #counter-value in counter.component.html Added increment counter e2e test case Removed examples created by Cypress installation

--- cypress/e2e/angular16-new-features/angular16-new-features.cy.js
+++ cypress/e2e/angular16-new-features/angular16-new-features.cy.js
@@ -1,6 +1,12 @@
 describe('Angular 16 new features e2e tests', () => {
-  it('first simple test of Cypress framwork with boolean value',
-    () => expect(true).to.equal(true)
-  )
+  it('should visit home page of Angular 16 new features home page', () => {
+    cy.visit('localhost:4200')
+  })
+
+  it('should increment counter', () => {
+    cy.visit('localhost:4200')
+    cy.get('#counter-augment').click();
+    cy.get('#counter-value').should('have.text','1');
+  })
 
 })
